6d150e7af25304500507c222b473d412af833e2a
gurgi414 <gurgithemastermind@gmail.com>
2019-11-25 23:21:47 -0500

    Final touch ups to code. Added time delays.

--- updater.py
+++ updater.py
@@ -1,28 +1,26 @@
 import subprocess
+import time
 
-fetchCommand = "git fetch --all"
+fetchCommand = "git fetch origin"
 diffCommand = "git diff origin/master"
-resetCommand = "git reset --hard origin/master"
-copyCommand = "sudo cp /home/pi/personalWebsite/. /var/www/html/"
+resetCommand = "git reset --hard origin/master && sudo cp /home/pi/personalWebsite/. /var/www/html/"
 
-fetchProcess = subprocess.run(fetchCommand.split(), cwd=r'/home/pi/personalWebsite', stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE, check=True,
-                              universal_newlines=True)
-diffProcess = subprocess.run(diffCommand.split(), cwd=r'/home/pi/personalWebsite', stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, check=True,
-                             universal_newlines=True)
-
-foutput = fetchProcess.stdout
-doutput = diffProcess.stdout
-
-if len(doutput) != 0:
-    print("need to update")
-    resetProcess = subprocess.run(resetCommand.split(), cwd=r'/home/pi/personalWebsite', stdout=subprocess.PIPE,
+while 1 == 1:
+    fetchProcess = subprocess.run(fetchCommand.split(), cwd=r'/home/pi/personalWebsite', stdout=subprocess.PIPE,
                                   stderr=subprocess.PIPE, check=True,
                                   universal_newlines=True)
-    routput = resetProcess.stdout
-    copyProcess = subprocess.run(copyCommand.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True,
+    time.sleep(1)
+    diffProcess = subprocess.run(diffCommand.split(), cwd=r'/home/pi/personalWebsite', stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE, check=True,
                                  universal_newlines=True)
-    coutput = copyProcess.stdout
-else:
-    print("no differences found")
+    time.sleep(0.4)
+    foutput = fetchProcess.stdout
+    doutput = diffProcess.stdout
+    if len(doutput) != 0:
+        print("need to update")
+        resetProcess = subprocess.run(resetCommand.split(), cwd=r'/home/pi/personalWebsite', stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE, check=True,
+                                      universal_newlines=True)
+        time.sleep(5)
+    else:
+        print("no differences found")
